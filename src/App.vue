f7b7026ff1538729f656280eb8489c8a9daeff33
Cesar Yesid Pineda <cpineda@automundial.com.co>
2019-01-03 17:50:50 -0500

    Fixes por la URL

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://cypineda76.github.io/platzi_music/dist/logo.png')
     h1 Platzi Music
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
