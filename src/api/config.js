24381018da35123082ab02eb6b54da2e4d937929
Cesar Yesid Pineda <cpineda@automundial.com.co>
2019-01-03 17:42:01 -0500

    project platzi

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appname: 'platzi_music',
+  apikey: '8854dcd2faa857f6a26f86d3ebad8018',
+  secret: 'ad6a5556f652b7bb4e6b8854bd4e06da',
+  registeredTo: 'cypineda76'
+}
+
+// Application name	platzi_music
+// API key	8854dcd2faa857f6a26f86d3ebad8018
+// Shared secret	ad6a5556f652b7bb4e6b8854bd4e06da
+// Registered to	cypineda76
